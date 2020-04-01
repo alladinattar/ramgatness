514693ce26ad4fe0670a6030da28c2202c9c737d
XuMuK666 <dedelya666@gmail.com>
2020-04-01 13:01:41 +0300

    update

--- loader.cpp
+++ loader.cpp
@@ -4,11 +4,23 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
-    int n = stoi(argv[1]);       
+    int n = stoi(argv[1]);  
+    int number = stoi(argv[2]);
     int i = 0;
+    unsigned char *pointer = (unsigned char*)malloc(n);
+    if (pointer==NULL) {
+    exit(1);
+    }
+    if (number == 1) {
     while(i<n){
-        malloc(1);
+        pointer[i] = 255;
         i++;
+    }
+    } else {
+        while(i<n){
+        pointer[i] = 0;
+        i++;
+    }
     }
     return 0;
 }
